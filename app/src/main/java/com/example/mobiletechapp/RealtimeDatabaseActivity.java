d5e43328c78b8976e41a68e3eec3cc7f4facae8f
Kusum Rijal <kusum@aryal.com.au>
2026-03-11 17:39:31 +1100

    firebase connected

---
+++ app/src/main/java/com/example/mobiletechapp/RealtimeDatabaseActivity.java
@@ -0,0 +1,46 @@
+package com.example.mobiletechapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RealtimeDatabaseActivity extends AppCompatActivity {
+
+    // Declare a database reference instance
+    DatabaseReference dbref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_realtime_database);
+
+        // Handle system bar padding (edge-to-edge UI)
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars =
+                            insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+                    return insets;
+                });
+
+        // Get reference to the Firebase Realtime Database
+        dbref = FirebaseDatabase.getInstance().getReference();
+
+        // Add data to database
+        dbref.child("testing").setValue("first test");
+    }
+}
